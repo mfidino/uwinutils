89c9778e8db9ec4cd9f6954bf9a5bf58ac77d351
Mason Fidino <mfidino@lpzoo.org>
2019-06-18 11:33:11 -0500

    add select query

--- R/database_functions.R
+++ R/database_functions.R
@@ -23,6 +23,8 @@
 #' @return If the password is correctly input \code{connect2db} will return
 #' a \code{MariaDBConnection} called \code{uwidb.}
 #'
+#' @importFrom rstudioapi askForPassword
+#'
 #' @example
 #'
 #' \dontrun{
@@ -34,8 +36,54 @@ connect2db <- function(){
             password = rstudioapi::askForPassword(prompt = 'Input UWIDB password'),
             host = '35.188.69.174',
             port = 3306,
-            database = 'UWIDB',
-            group = 'UWIDB')
+            dbname = 'UWIDB')
   assign('uwidb', uwidb, envir = globalenv())
+}
+
+#' Apply a select query to the UWIN database.
+#'
+#' \code{SELECT} will send SELECT queries to the UWIN database and return
+#' the output.
+#'
+#' @param sql A SQL statement input as a character vector to be sent to the
+#'   UWIN database.
+#'
+#' @param db The MariaDB connection to the UWIN database. Defaults to 'uwidb'
+#'
+#' @return A data.frame with the output from the SQL statement.
+
+#'
+#' @example
+#'
+#' \dontrun{
+#' my_sql <- 'SELECT * FROM Visits'
+#' SELECT(my_sql)
+#' }
+
+# helper functions
+SELECT <- function(sql = NULL, db = uwidb){
+  if(!is.character(sql)){
+    stop('sql must be a character object')
+  }
+  if(class(db) != 'MariaDBConnection'){
+    stop('db is not the correct class, please connect to database with connect2db().')
+  }
+  qry <- dbSendQuery(db, sql)
+  result <- dbFetch(qry)
+  dbClearResult(qry)
+  if(nrow(result) == 0){
+    stop(paste0("The query returned 0 records\n\n", sql))
+  }
+  return(result)
+}
+
+update_qry <- function(sql = NULL, report = FALSE, db = uwidb){
+  qry <- RMariaDB::dbSendStatement(uwidb, sql)
+  if(report){
+    to_report <- dbGetRowsAffected(qry)
+    dbClearResult(qry)
+    return(to_report)
+  }
+  dbClearResult(qry)
 }
 
